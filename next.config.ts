cee6d2efbf4b427ec7762fc2f2f614f9825c256f
Aristarh <aristiktop8@gmail.com>
2026-04-08 19:14:39 +0500

    next-config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/libs/i18n/request.ts");
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
